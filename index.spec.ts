a2808f5ad21ef01938c0e73d09d077c49bb3d7c8
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2024-02-27 14:01:36 +0100

    Improve section splitting

--- index.spec.ts
+++ index.spec.ts
@@ -4,7 +4,7 @@ import { getMcc } from "./index"
 describe("library", () => {
 	it("get all mcc", () => {
 		// const index = 2
-		const mccs = getMcc(22, 23)
+		const mccs = getMcc()
 		console.log(mccs.length)
 		console.log(JSON.stringify(mccs, null, 2))
 	})
--- splitBySections.ts
+++ splitBySections.ts
@@ -3,7 +3,7 @@ import { groupLinesByCondition } from "./utilities"
 export function splitBySections(chapter: string) {
 	return groupLinesByCondition(
 		chapter,
-		line => line.match(/^MCC \d{4}: (\w| |,|;|:)+$/g) || line.match(/MCCs \d{4} through \d{4}: (\w| |,)+/g),
+		line => line.match(/^MCC \d{4}: [^\n]+$/g) || line.match(/MCCs \d{4} through \d{4}: [^\n]+/g),
 		{ includeFirst: false }
 	)
 }
